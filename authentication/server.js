1e0c08fc5c9e82a3548f8d79c6030349a8cb59e5
aashrikaa <134730633+aashrikaa@users.noreply.github.com>
2023-06-12 18:59:07 -0700

    server.js

--- authentication/server.js
+++ authentication/server.js
@@ -1,39 +1,95 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 const mysql = require('mysql');
+const jwt = require('jsonwebtoken');
+const bcrypt = require('bcrypt');
 
 const app = express();
 
-// Create a MySQL connection
+const port = process.env.PORT || 3000;
+
+app.use(bodyParser.json());
+
 const connection = mysql.createConnection({
   host: 'localhost',
   user: 'root',
-  password: 'password',
+  password: '',
   database: 'dummy_db'
 });
 
-// Connect to the database
+
 connection.connect((err) => {
   if (err) {
-    console.error('Error connecting to MySQL database: ', err);
+    console.error('Error connecting to database: ', err);
     return;
   }
-  console.log('Connected to MySQL database');
+  console.log('Connected to database!');
 });
 
-// Define the routes for sign up and sign in
 app.post('/signup', (req, res) => {
-  // Implement role-based authorization logic for sign up
-  // ...
-  res.send('Sign up successful');
+  const { username, password, role } = req.body;
+  bcrypt.hash(password, 10, (err, hash) => {
+    if (err) {
+      console.error('Error hashing password: ', err);
+      res.status(500).send('Internal server error');
+      return;
+    }
+   
+    const user = { username, password: hash, role };
+  
+    connection.query('INSERT INTO users SET ?', user, (err, result) => {
+      if (err) {
+        console.error('Error inserting user into database: ', err);
+        res.status(500).send('Internal server error');
+        return;
+      }
+      console.log('User signed up successfully!');
+      res.status(201).send('User signed up successfully!');
+    });
+  });
 });
+
 
 app.post('/signin', (req, res) => {
-  // Implement role-based authorization logic for sign in
-  // ...
-  res.send('Sign in successful');
+
+  const { username, password } = req.body;
+ 
+  connection.query('SELECT * FROM users WHERE username = ?', username, (err, results) => {
+    if (err) {
+      console.error('Error selecting user from database: ', err);
+      res.status(500).send('Internal server error');
+      return;
+    }
+    
+    if (results.length === 0) {
+      console.log('User not found!');
+      res.status(401).send('Invalid username or password');
+      return;
+    }
+    
+    const user = results[0];
+    
+    bcrypt.compare(password, user.password, (err, result) => {
+      if (err) {
+        console.error('Error comparing passwords: ', err);
+        res.status(500).send('Internal server error');
+        return;
+      }
+      
+      if (!result) {
+        console.log('Invalid password!');
+        res.status(401).send('Invalid username or password');
+        return;
+      }
+      
+      const token = jwt.sign({ username, role: user.role }, 'secret');
+      console.log('User signed in successfully!');
+      // Return the JWT token in the response body
+      res.status(200).json({ token });
+    });
+  });
 });
 
-// Start the server
-app.listen(3000, () => {
-  console.log('Server started on port 3000');
+app.listen(port, () => {
+  console.log(`Server listening on port ${port}`);
 });
