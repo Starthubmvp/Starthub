2c8b74f9d0602453bb3437699c7c29b84a13092f
aashrikaa <134730633+aashrikaa@users.noreply.github.com>
2023-07-14 21:36:10 +0545

    Delete backend directory

--- backend/routes/pay.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const ctrl = require('../controllers');
-
-router.post('/createCustomer', ctrl.pay.createCustomer);
-router.post('/createPaymentIntent', ctrl.pay.createPaymentIntent);
-
-
-module.exports = router;
--- backend/routes/user.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const ctrl = require('../controllers');
-
-router.get('/:id', ctrl.user.show);
-
-
-module.exports = router;
