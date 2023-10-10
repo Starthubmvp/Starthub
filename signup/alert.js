c38befb420ca3206e8c804edebe73d36718d5cae
aashrikaa <134730633+aashrikaa@users.noreply.github.com>
2023-10-10 23:11:45 +0545

    Update alert.js

--- signup/alert.js
+++ signup/alert.js
@@ -1,12 +1,1 @@
-import { BiErrorCircle } from "react-icons/bi";
 
-const Alert = (props) => {
-  return (
-    <div className="flex items-center justify-center px-2 py-2  mb-1 bg-lightRedColor rounded-sm  ">
-      <BiErrorCircle className="text-darkRedColor" />
-      <p className="text-sm pl-1 text-darkRedColor">{props.message}</p>
-    </div>
-  );
-};
-
-export default Alert;
